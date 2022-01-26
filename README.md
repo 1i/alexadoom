44b494596db6240e8fad9986a86a29af7ae3888d
Patrick O'Sullivan <patrick.osullivan@synchronoss.com>
2022-01-26 15:43:28 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # alexadoom
+
+
+Alexa skill to have Alexa whisper the ,I Dogma, intro to the Doom 2016 game.
+https://www.youtube.com/watch?v=nMd--Wyfw0g
--- alexa.go
+++ alexa.go
@@ -23,7 +23,6 @@ func Handler() (alexa.Response, error) {
 	fmt.Sprintf("response : %T/n", response)
 	fmt.Print("end ")
 	return response, nil
-	//return alexa.NewSimpleResponse("Saying Hello", "<speak><amazon:effect name='whispered'>" + "In the first age, in the first battle, when the shadows first lengthened, one stood. He chose the path of perpetual torment. In his ravenous hatred he found no peace. And with boiling blood he scoured the Umbral Plains seeking vengeance against the dark lords who had wronged him. And those that tasted the bite of his sword named him... the Doom Slayer.  " + "</amazon:effect></speak>"), nil
 }
 
 func main() {
